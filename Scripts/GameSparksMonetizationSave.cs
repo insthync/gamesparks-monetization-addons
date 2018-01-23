0cbea0f587c94a084d970ddb05fe38ae27c4e1b0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-23 14:48:24 +0700

    WIP, connect to service to set data/ get data, avoid duplicate requesting.

--- Scripts/GameSparksMonetizationSave.cs
+++ Scripts/GameSparksMonetizationSave.cs
@@ -12,13 +12,25 @@ public class GameSparksMonetizationSave : BaseMonetizationSave
 {
     public static readonly Dictionary<string, int> CurrencyAmounts = new Dictionary<string, int>();
     public static PurchasedItems Items = new PurchasedItems();
+    public static bool IsRequestedCurrencyOnce { get; private set; }
+    public static bool IsRequestedItemsOnce { get; private set; }
+    public static bool IsRequestingCurrencyService { get; private set; }
+    public static bool IsRequestingItemsService { get; private set; }
     private void Awake()
     {
+        IsRequestedCurrencyOnce = false;
+        IsRequestedItemsOnce = false;
+        IsRequestingCurrencyService = false;
+        IsRequestingItemsService = false;
         RequestCurrencyData();
     }
 
     public void RequestCurrencyData()
     {
+        if (IsRequestingCurrencyService)
+            return;
+        IsRequestingCurrencyService = true;
+
         var currencyKeys = new List<string>(MonetizationManager.Currencies.Keys);
         var currencyAmounts = new int[6];
         new AccountDetailsRequest().Send((response) => {
@@ -35,7 +47,26 @@ public class GameSparksMonetizationSave : BaseMonetizationSave
                 CurrencyAmounts[currencyKeys[i]] = currencyAmounts[i];
                 ++i;
             }
+
+            IsRequestedCurrencyOnce = true;
+            IsRequestingCurrencyService = false;
         });
+    }
+
+    public void RequestItemData()
+    {
+        if (IsRequestingItemsService)
+            return;
+        IsRequestingItemsService = true;
+
+        var keys = new List<string>(CurrencyAmounts.Keys);
+        new LogEventRequest().SetEventKey("SERVICE_EVENT")
+            .SetEventAttribute("TARGET", "getPurchasedItems")
+            .Send((response) =>
+            {
+                IsRequestedItemsOnce = true;
+                IsRequestingItemsService = false;
+            });
     }
 
     public override bool AddCurrency(string name, int amount)
@@ -69,39 +100,59 @@ public class GameSparksMonetizationSave : BaseMonetizationSave
 
     public override void SetCurrency(string name, int amount)
     {
+        if (!IsRequestedCurrencyOnce)
+            return;
+
+        if (!CurrencyAmounts.ContainsKey(name) || CurrencyAmounts[name] == amount)
+            return;
+
+        if (IsRequestingCurrencyService)
+            return;
+        IsRequestingCurrencyService = true;
+
         var keys = new List<string>(CurrencyAmounts.Keys);
-        var json = "{ \"currencyIndex\" : \"" + keys.IndexOf(name) + "\", \"currencyAmount\" : \"" + amount + "\" }";
+        var json = string.Format("{ \"currencyIndex\" : \"{0}\", \"currencyAmount\" : \"{1}\" }", keys.IndexOf(name), amount);
         new LogEventRequest().SetEventKey("SERVICE_EVENT")
             .SetEventAttribute("TARGET", "setCurrency")
-            .SetEventAttribute("DATA", json)
+            .SetEventAttribute("DATA", new GSRequestData(json))
             .Send((response) =>
             {
                 if (!response.HasErrors)
                     CurrencyAmounts[name] = amount;
+
+                IsRequestingCurrencyService = false;
             });
     }
 
     public override void SetPurchasedItems(PurchasedItems purchasedItems)
     {
+        if (!IsRequestedItemsOnce)
+            return;
+
         if (purchasedItems == null)
             return;
 
-        var json = "[";
+        if (IsRequestingItemsService)
+            return;
+        IsRequestingItemsService = true;
+
+        var items = "";
         foreach (var itemName in Items.itemNames)
         {
-            if (json.Length > 1)
-                json += ",";
-            json += "\"" + itemName + "\"";
+            if (items.Length > 1)
+                items += ",";
+            items += "\"" + itemName + "\"";
         }
-        json += "]";
-        json = "{ \"items\" : " + json + " }";
+        var json = string.Format("{ \"items\" : [{0}] }", items);
         new LogEventRequest().SetEventKey("SERVICE_EVENT")
             .SetEventAttribute("TARGET", "setPurchasedItems")
-            .SetEventAttribute("DATA", json)
+            .SetEventAttribute("DATA", new GSRequestData(items))
             .Send((response) =>
             {
                 if (!response.HasErrors)
                     Items = purchasedItems;
+
+                IsRequestingItemsService = false;
             });
     }
 }
